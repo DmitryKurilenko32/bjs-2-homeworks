b1b24c6ef303f85775276ceb92741ff130984dd5
Dmitry <kurilenko907@gmail.com>
2024-10-22 23:03:44 +0300

    task arrays finish

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+    const result = arr1.length === arr2.length && arr1.every ((element , i) => element === arr2[i]);
+       return result
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    const filterUsers = users.filter(user => user.gender === gender);
+
+    if (filterUsers.length === 0) {
+        return 0;
+    } 
+    const age = filterUsers.map( user => user.age);
+
+     return average = age.reduce((sum , age) => (sum + age)) / filterUsers.length;
+    
+    
+} 
